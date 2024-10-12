86bf81a0d613ed48a737873bcc66e61be986abda
Kássio San <kciosantana@gmail.com>
2024-10-12 20:09:15 -0300

    out put user pool info

--- cognito/cognito.tf
+++ cognito/cognito.tf
@@ -14,3 +14,11 @@ resource "aws_cognito_user_pool" "pool" {
     required = false
   }
 }
+
+output "user_pool_id" {
+  value = aws_cognito_user_pool.pool.id
+}
+
+output "client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
